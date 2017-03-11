1ff72f772318f04e2062ae24e05f8dfe7caa4d7d
douglas aquilino <douglas aquilino>
2017-03-11 13:20:53 -0500

    changed limit from 100 to 20

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ $GRANT_TYPE = "client_credentials";
 // Defaults for our simple example.
 $DEFAULT_TERM = "dinner";
 $DEFAULT_LOCATION = "San Francisco, CA";
-$SEARCH_LIMIT = 100;
+$SEARCH_LIMIT = 20;
 
 /**
  * Given a bearer token, send a GET request to the API.
